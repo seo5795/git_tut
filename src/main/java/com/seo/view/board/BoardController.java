0385a50998f15caa061281d805a17626316755e9
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-07-21 15:25:42 +0900

    BoardController Annotation 수정

--- src/main/java/com/seo/view/board/BoardController.java
+++ src/main/java/com/seo/view/board/BoardController.java
@@ -3,6 +3,7 @@ package com.seo.view.board;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,65 +12,56 @@ import com.seo.myapp.board.impl.BoardDAO;
 
 @Controller
 public class BoardController {
-	@RequestMapping("/insertBoard.do")
+	@RequestMapping(value="/insertBoard.do")
 	public String insertBoard(BoardVO vo,BoardDAO dao) {
 		System.out.println("메시지 등록중....");	
 		dao.insertBoard(vo);		
-		return "index.jsp";
+		return "getBoardList.do";
 	}
 	
 	@RequestMapping("/updateBoard.do")
-	public ModelAndView updateBoard(BoardVO vo,BoardDAO dao,ModelAndView mav) {
+	public String updateBoard(BoardVO vo,BoardDAO dao) {
 		System.out.println("updateBoardController 실행중");
 		dao.updateBoard(vo);	
-		mav.setViewName("getBoardList.do");
-		return mav;
+		return "getBoardList.do";
 	}
 	
 	@RequestMapping("/deleteBoard.do")
-	public ModelAndView deleteBoard(BoardVO vo,BoardDAO dao,ModelAndView mav) {
+	public String deleteBoard(BoardVO vo,BoardDAO dao) {
 		System.out.println("DeleteBoardController 실행중");
 		dao.deleteBoard(vo);
-		mav.setViewName("getBoardList.do");
-		return mav;
+		
+		return "getBoardList.do";
 	}
 	
 	@RequestMapping("/getBoard.do")
-	public ModelAndView getBoard(BoardVO vo,BoardDAO dao,ModelAndView mav) {
+	public String getBoard(BoardVO vo,BoardDAO dao,Model m) {
 		System.out.println("GetBoardController 수행중");
 
 		BoardVO v=dao.getBoard(vo);
-		mav.addObject("v", v);
-		mav.setViewName("board.jsp");
-		return mav;
+		m.addAttribute("v",v);
+	
+		return "board.jsp";
 	}
 	
 	@RequestMapping("/getBoardList.do")
-	public ModelAndView getBoardList(BoardVO vo,BoardDAO dao,ModelAndView mav) {
+	public String getBoardList(BoardVO vo,BoardDAO dao,Model m) {
 		System.out.println("GetBoardListController 실행중");
 
 		List<BoardVO> boardList=dao.getBoardList(); // 추후에 검색기능을 추가해볼수있겠다!
 	
-		mav.addObject("boardList", boardList);
-		mav.setViewName("boardList.jsp");
-		return mav;
-	}
-	@RequestMapping("/UpdateBoard.do")
-	public ModelAndView UpdateBoard(BoardVO vo,BoardDAO dao,ModelAndView mav) {
-		System.out.println("updateBoardController 실행중");
-		dao.updateBoard(vo);
-
+		m.addAttribute("boardList",boardList);
 		
-		mav.setViewName("getBoardList.do");
-		return mav;
+		return "boardList.jsp";
 	}
+	
 	@RequestMapping("/updateBoardPage.do")
-	public ModelAndView updateBoardPage(BoardVO vo,BoardDAO dao,ModelAndView mav) {
+	public String updateBoardPage(BoardVO vo,BoardDAO dao,Model m) {
 		System.out.println("UpdateBoardPageController 수행중");
 		BoardVO v=dao.getBoard(vo);
 
-		mav.addObject("v", v);
-		mav.setViewName("updateBoard.jsp");
-		return mav;
+		m.addAttribute("v",v);
+		
+		return "updateBoard.jsp";
 	}
 }
