d728907d3091e7d5070c59f065167bffcfce86bc
seo5795 <85172654+seo5795@users.noreply.github.com>
2021-07-25 10:36:20 +0900

    예외처리

--- src/main/java/com/seo/view/users/UserController.java
+++ src/main/java/com/seo/view/users/UserController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.seo.myapp.users.UsersService;
 import com.seo.myapp.users.UsersVO;
@@ -16,9 +18,14 @@ public class UserController {
 	private UsersService usersService;
 	//비즈니스 컴포넌트, dao바꾸기 용이해짐
 	
-	@RequestMapping(value="/login.do")
+	@RequestMapping(value="/login.do",method=RequestMethod.POST)
 	public String login(UsersVO vo,HttpSession session) {
 		System.out.println("로그인 컨트롤러");
+		
+		if(vo.getId()==null || vo.getId().equals("")) {
+			throw new IllegalArgumentException("아이디가 입력되지않았습니다.");
+		}
+		
 		UsersVO data=usersService.getUsers(vo);
 		System.out.println("getUsers끝");
 		if(data!=null) {
@@ -29,6 +36,18 @@ public class UserController {
 			return "login.jsp";
 		}
 	}
+	
+	
+	@RequestMapping(value="/login.do",method=RequestMethod.GET)
+	public String login2(@ModelAttribute("user")UsersVO vo) {
+		System.out.println("로그인화면을 서비스하는 메서드동작함");
+		
+		vo.setId("a");
+		vo.setPw("a");
+		
+		return "login.jsp";
+	}
+	
 	@RequestMapping("logout.do")
 	public String logout(HttpSession session) {
 		System.out.println("로그아웃 컨트롤러");
